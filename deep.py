05e1c9552bcce177419b3a1f1ff071eb4160902b
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-03 14:37:42 -0700

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,6 +1,10 @@
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-if answer == 42 or answer.lower() == "fourty-two" or answer.lower() == "fourty two":
+if answer == 42:
     print("Yes")
+elif answer.lower() == "fourty-two":
+    print("Yes")
+elif answer.lower() == "fourty two":
+
 else:
     print("No")
