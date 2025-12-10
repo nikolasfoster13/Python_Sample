be61fcd42d48ac5383f7a7795430061fd3c37b26
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-10 09:01:28 -0700

    automated commit by submit50

--- interpreter.py
+++ interpreter.py
@@ -1,20 +1,27 @@
+# Prompt user for expression
 expr = input("Expression: ").strip()
+# Split expression into 3 parts, x, operator, y
 variables = expr.split(" ")
+# If operator is + add x and y, print answer as rounded to 1 decimal
 if variables[1] == "+":
     value = float(float(variables[0])+float(variables[2]))
     rounded = round(value,2)
     print(rounded)
+# If operator is - subtract y from x, print answer as rounded to 1 decimal
 elif variables[1] == "-":
     value = float(float(variables[0])-float(variables[2]))
     rounded = round(value,2)
     print(rounded)
+# If operator is /, divide x by y, print answer as rounded to 1 decimal
 elif variables[1] == "/":
     value = float(float(variables[0])/float(variables[2]))
     rounded = round(value,2)
     print(rounded)
+# If operator is *, multiply x by y, print answer as rounded to 1 decimal
 elif variables[1] == "*":
     value = float(float(variables[0])*float(variables[2]))
     rounded = round(value,2)
     print(rounded)
+# If expression is not formatted properly, reprompt
 else:
     pass
