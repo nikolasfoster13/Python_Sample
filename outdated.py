918bc4873b9413b8c5995f4fba1f17f03557c57f
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-10 08:22:18 -0700

    automated commit by check50 [check50=True]

--- outdated.py
+++ outdated.py
@@ -15,50 +15,16 @@ while True:
     user = input("Date: ")
     alpha_check = user.split(" ")
     num_check = user.split("/")
-    if alpha_check[0].isalpha() == True and alpha_check[0] in months:
-        if int(alpha_check[1].rstrip(",")) <= 31:
-            year = alpha_check[2]
-            month = months[alpha_check[0]]
-            day= alpha_check[1].rstrip(",").rjust(2,"0")
-            print(f"{year}-{month}-{day}")
-            break
-        else:
-            if num_check[0].isnumeric() == True:
-                if int(num_check[0]) <= 12:
-                    if int(num_check[1]) <= 31:
-                        year = num_check[2]
-                        month = num_check[0],rjust(2,"0")
-                        day = num_check[1],rjust(2,"0")
-                        print(f"{year}-{month}-{day}")
-                        break
-                    else:
-                        raise ValueError
-                else:
-                    raise ValueError
-            else:
-                raise ValueError
+    if alpha_check[0].isalpha() == True and alpha_check[0] in months and int(alpha_check[1].rstrip(",")) <= 31:
+        year = alpha_check[2]
+        month = months[alpha_check[0]]
+        day= alpha_check[1].rstrip(",").rjust(2,"0")
+        print(f"{year}-{month}-{day}")
+    elif num_check[0].isnumeric() == True and int(num_check[0]) <= 12 and int(num_check[1]) <= 31:
+        year = num_check[2]
+        month = num_check[0].rjust(2,"0")
+        day = num_check[1].rjust(2,"0")
+        print(f"{year}-{month}-{day}")
+        break
     else:
-        if ValueError:
-            pass
-        else:
-            pass
-
-
-
-
-
-    #         if ValueError:
-    #             pass
-    #         else:
-    #             pass
-    # # else:
-    #     if int(num_check[0].isnumeric()) <= 12:
-    #         if int(num_check[1]) <= 31:
-    #             year = num_check[2]
-    #             month = num_check[0].rjust(2,"0")
-    #             day = num_check[1].rjust(2,"0")
-    #             print(f"{year}-{month}-{day}")
-    #             break
-    #         else:
-    #             pass
-
+        pass
