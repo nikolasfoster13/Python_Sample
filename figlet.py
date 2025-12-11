a30de38eabf363e0b110fc718e3297f0b7081731
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-11 14:49:22 -0700

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -1,9 +1,9 @@
 from pyfiglet import Figlet
-from sys import argv, exit
+import sys
 from random import choice
 
 # If user runs program w/o declaring font
-if len(argv) == 1:
+if len(sys.argv) == 1:
     figlet = Figlet()
     # Generate random font
     figlet.setFont(font = choice(figlet.getFonts()))
@@ -13,9 +13,9 @@ if len(argv) == 1:
     print("Output: \n",figlet.renderText(user))
 
 # If user declares font and provides a valid font
-elif (len(argv) == 3) and (argv[1] == "-f" or argv[1] == "--font") and (argv[2] in Figlet().getFonts()):
+elif (len(sys.argv) == 3) and (sys.argv[1] == "-f" or sys.argv[1] == "--font") and (sys.argv[2] in Figlet().getFonts()):
     figlet = Figlet()
-    figlet.setFont(font=argv[2])
+    figlet.setFont(font=sys.argv[2])
     # Prompt for input
     user = input("Input: ")
     # Print output in input font
@@ -24,6 +24,6 @@ elif (len(argv) == 3) and (argv[1] == "-f" or argv[1] == "--font") and (argv[2] 
 # Else, exit program
 else:
     print("Invalid usage")
-    exit
+    sys.exit
 
 
