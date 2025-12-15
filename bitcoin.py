3617668bc91ec6d78e54e243472a347e25f78864
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-15 15:47:39 -0700

    automated commit by check50 [check50=True]

--- bitcoin.py
+++ bitcoin.py
@@ -4,19 +4,25 @@ import json
 
 while True:
     try:
+        # Ensure command-line argument has 1 parameter
         if len(sys.argv) != 2:
+            # Inform user of missing command-line argument and exit
             sys.exit("Missing command-line argument")
 
-        elif int(sys.argv[1]) < 1:
-            raise ValueError
-
         else:
+            # Call coincap API for current price
             response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=297fee44b270cbe1131a8e7b158dd23fec0805dbd5ea83cd57653010dcb7baa6")
+            # Format to JSON dict
             o = response.json()
+            # Extract price from dict
             price = o["data"]["priceUsd"]
+            # Multiply price by number prompt
             cost = float(sys.argv[1]) * float(price)
-            sys.exit(f"${cost:,.4f}")
+            # Print price formatted with , sep thousands to 4 decimals
+            print(f"${cost:,.4f}")
+            sys.exit()
 
+    # If CL argument is non numeric, return error and exit
     except ValueError:
         sys.exit("Command-line argument is not a number")
 
