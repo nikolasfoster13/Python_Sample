9f379f6a68fd75aaba65d7d3cd440590eefc1e69
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-16 09:09:15 -0700

    automated commit by check50 [check50=True]

--- test_plates.py
+++ test_plates.py
@@ -2,24 +2,29 @@ from plates import is_valid
 
 def test_start():
     assert is_valid("11") == False
-
+    assert is_valid("1A") == False
+    assert is_valid("AA") == True
 
 def test_length():
     assert is_valid("A") == False
-    assert is_valid("AAAAAAAA") == False
     assert is_valid("AA") == True
+    assert is_valid("AAA") == True
+    assert is_valid("AAAA") == True
+    assert is_valid("AAAAA") == True
+    assert is_valid("AAAAAA") == True
+    assert is_valid("AAAAAAA") == True
 
 def test_is_valid_non_an():
-    assert is_valid("_A") == False
-    assert is_valid("AA") == True
+    assert is_valid("AA!") == False
+    assert is_valid("AAA") == True
 
 def test_is_valid_middle_num():
     assert is_valid("A2A") == False
-    assert is_valid("AA") == True
+    assert is_valid("AA2") == True
 
 def test_is_valid_zero_start():
     assert is_valid("A02") == False
-    assert is_valid("A2") == True
+    assert is_valid("AA2") == True
 
 def test_is_valid():
     assert is_valid("AA") == True
