e72f78469c079ed1613ad915510a232329960ae4
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-16 08:27:15 -0700

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -1,11 +1,11 @@
 from bank import value
 
 def test_bank_hello():
-    assert value("Hello").lower() == "$0"
+    assert value("Hello") == 0
 
 def test_bank_hi():
-    assert value("Hi").lower() == "$20"
+    assert value("Hi") == 20
 
 def test_bank_whats_up():
-    assert value("What's up") == "$100"
-    
+    assert value("What's up") == 100
+
