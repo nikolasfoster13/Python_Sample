e153477bba82ec515837ce42b2c3caf1c17640c0
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-16 08:12:33 -0700

    automated commit by check50 [check50=True]

---
+++ test_twttr.py
@@ -0,0 +1,34 @@
+import twttr
+
+def test_twttr_little_a():
+    assert twttr.shorten("cat") == "ct"
+
+def test_twttr_big_a():
+    assert twttr.shorten("CAT") == "CT"
+
+def test_twttr_little_e():
+    assert twttr.shorten("egg") == "gg"
+
+def test_twttr_big_e():
+    assert twttr.shorten("EGG") == "GG"
+
+def test_twttr_little_i():
+    assert twttr.shorten("rip") == "rp"
+
+def test_twttr_big_i():
+    assert twttr.shorten("RIP") == "RP"
+
+def test_twttr_little_O():
+    assert twttr.shorten("cop") == "cp"
+
+def test_twttr_big_o():
+    assert twttr.shorten("COP") == "CP"
+
+def test_twttr_little_u():
+    assert twttr.shorten("cup") == "cp"
+
+def test_twttr_big_u():
+    assert twttr.shorten("CUP") == "CP"
+
+def test_twttr_no_v():
+    assert twttr.shorten("RTLP") == "RTLP"
