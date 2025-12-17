61c50e17923876d8b7f6577a17a128aeeac1a5ef
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2025-12-17 09:45:57 -0700

    automated commit by check50 [check50=True]

--- pizza.py
+++ pizza.py
@@ -9,14 +9,14 @@ file_name,file_type = sys.argv[1].split(".")
 if file_type != "csv":
     sys.exit("Invalid file type")
 try:
-    headers = []
     menu = []
     with open(f"{sys.argv[1]}") as file:
+        first_line = file.readline()
+        headers = [h.strip() for h in first_line.rstrip().split(",")]
         for line in file:
-            item,small,large = line.rstrip().split(",")
-            items = {"item":item, "small":small, "large":large}
-            menu.append(items)
-    print(tabulate.tabulate(menu,tablefmt="grid"))
+            item,small,large = [x.strip() for x in line.rstrip().split(",")]
+            menu.append([item, small, large])
+    print(tabulate.tabulate(menu,headers = headers,tablefmt="grid"))
 except FileNotFoundError:
     sys.exit("File does not exist")
 
