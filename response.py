3bf63a08f4f97c1159765c4a6da74eeec658a75b
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2026-01-05 09:37:47 -0700

    automated commit by check50 [check50=True]

--- response.py
+++ response.py
@@ -1,17 +1,23 @@
-from validator_collection import validators
+from validator_collection import validators, errors
 
+#Prompt user for email and return valid or invalid
 def main():
    print(validate(input("What's your email address? ")))
 
 
 def validate(s):
+    # Run email through validators.email
     try:
+        # If None, return Invalid
         if validators.email(s) == None:
             return "Invalid"
+        # Else, return Valid
         else:
             return "Valid"
-    except (ValueError, EmptyValueError):
+    # Return errors as invalid
+    except (ValueError, errors.EmptyValueError):
         return "Invalid"
+
 if __name__ == "__main__":
     main()
 
