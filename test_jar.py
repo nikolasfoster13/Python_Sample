cf1c14955d60274dd25bbc985bf68dd7e49afea9
nikolasfoster13 <nikolasfoster13@users.noreply.github.com>
2026-01-06 14:55:44 -0700

    automated commit by check50 [check50=True]

---
+++ test_jar.py
@@ -0,0 +1,28 @@
+from jar import Jar
+import pytest
+
+def test_init():
+    jar = Jar()
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(6)
+    assert jar._size == 6
+    with pytest.raises(ValueError):
+        jar.deposit(100)
+
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(6)
+    jar.withdraw(1)
+    assert jar._size == 5
+    with pytest.raises(ValueError):
+        jar.withdraw(100)
